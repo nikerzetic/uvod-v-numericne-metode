7e57a7a94f576279569534efa7e102c129139e4b
Nik Erzetic <nikerzetic@gmail.com>
2019-11-23 16:56:10 +0100

    Koncan thomas

--- thomas.m
+++ thomas.m
@@ -3,7 +3,7 @@ function [x,l,u] = thomas(a,b,c,z)
     % Thomasov postopek za resevanje sistema s tridiagonalno matriko z
     % LU razcepom brez pivotiranja
     
-    n = size(a);
+    n = size(a,1);
     
     l = zeros(n-1,1);
     u = zeros(n,1);
@@ -13,7 +13,7 @@ function [x,l,u] = thomas(a,b,c,z)
     for i = 1:n-1
        
         l(i) = c(i) / a(i);
-        u(i) = c(i) - l(i-1)*b(i-1);
+        u(i+1) = c(i) - l(i)*b(i);
         
     end
     
@@ -21,10 +21,18 @@ function [x,l,u] = thomas(a,b,c,z)
     
     for i = 2:n
        
-        y(i) = b(i) - l(i-1)*y(i-1);
+        y(i) = b(i-1) - l(i-1)*y(i-1);
         
     end
     
+    x = y;
     
+    x(n) = x(n) / u(n);
+    
+    for i = n-1:-1:1
+    
+        x(i) = (y(i) - b(i)*x(i+1)) / u(i);
+    
+    end
     
 end
