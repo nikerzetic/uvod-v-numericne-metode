626fd1ce5b8c393b615d546bd198bfdef9e3f06a
Nik Erzetic <nikerzetic@gmail.com>
2019-11-30 12:22:40 +0100

    Popravljen thomas

--- thomas.m
+++ thomas.m
@@ -1,38 +1,30 @@
 function [x,l,u] = thomas(a,b,c,z)
 
-    % Thomasov postopek za resevanje sistema s tridiagonalno matriko z
-    % LU razcepom brez pivotiranja
-    
-    n = size(a,1);
-    
-    l = zeros(n-1,1);
-    u = zeros(n,1);
+% Thomasov postopek za resevanje sistema s tridiagonalno matriko z
+% LU razcepom brez pivotiranja
 
-    u(1) = a(1);
-    
-    for i = 1:n-1
-       
-        l(i) = c(i) / a(i);
-        u(i+1) = c(i) - l(i)*b(i);
-        
-    end
-    
-    y = z;
-    
-    for i = 2:n
-       
-        y(i) = b(i-1) - l(i-1)*y(i-1);
-        
-    end
-    
-    x = y;
-    
-    x(n) = x(n) / u(n);
-    
-    for i = n-1:-1:1
-    
-        x(i) = (y(i) - b(i)*x(i+1)) / u(i);
-    
-    end
+n = size(a,1);
+l = zeros(n-1,1);
+u = zeros(n,1);
+
+% LU razcep
+u(1) = a(1);
+for i = 1:n-1
+    l(i) = c(i) / a(i);
+    u(i+1) = a(i+1) - l(i)*b(i);
+end
+
+% prema substitucija
+y = z;
+for i = 2:n
+    y(i) = z(i) - l(i-1)*y(i-1);
+end
+
+% obratna substitucija
+x = y;
+x(n) = x(n) / u(n);
+for i = n-1:-1:1
+    x(i) = (y(i) - b(i)*x(i+1)) / u(i);
+end
     
 end
